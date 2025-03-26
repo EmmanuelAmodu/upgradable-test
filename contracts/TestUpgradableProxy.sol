50e81d7c4abaad8b47483e45ed3379f89b0f3f94
Emmanuel Amodu <emmanuel.amodu@outlook.com>
2025-03-26 17:46:08 +0100

    refactor: move upgradeable contract implementation to contracts directory and remove old test files

--- contracts/TestUpgradableProxy.sol
+++ contracts/TestUpgradableProxy.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts-upgradeable/proxy/ERC1967/ERC1967Proxy.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 /**
  * @title TestUpgradableProxy
